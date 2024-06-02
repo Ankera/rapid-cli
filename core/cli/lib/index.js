4b23cf890dcb48fb46a4c5f8565b9ed0178e15a4
ankera <15189120919@163.com>
2024-06-02 11:07:20 +0800

    feat: 检查用户主目录

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,6 +1,8 @@
 const pkg = require('../package.json');
 const utils = require('@rapid-cli/utils');
 const log = require('@rapid-cli/log');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
 const semver = require('semver');
 const colors = require('colors');
 const constant = require('./const');
@@ -10,6 +12,10 @@ function core() {
     checkPkgVersion();
   
     checkNodeVersion();
+
+    checkRoot();
+
+    checkUserHome();
   } catch (error) {
     log.error(error.message)
   }
@@ -26,6 +32,24 @@ function checkNodeVersion() {
 
   if (!semver.gte(currentVersion, lowestNodeVersion)) {
     throw new Error(colors.red(`Rapid-CLI 需要安装 v${lowestNodeVersion} 以上版本的Node.JS`))
+  }
+}
+
+/**
+ * sudo rp 启动的命令 process.geteuid() 就是 0
+ * rp 启动的命令 process.geteuid() 普通用户 就是 501
+ * 
+ * process.setegid()
+ * process.seteuid()
+ */
+function checkRoot() {
+  const rootCheck = require('root-check');
+  rootCheck();
+}
+
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('用户主目录不存在'));
   }
 }
 
